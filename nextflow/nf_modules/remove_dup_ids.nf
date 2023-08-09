d8062d0e7b2eb78620b1d5051317dee579637066
Syed Nakib Hossain <snhossain@ebi.ac.uk>
2023-08-09 14:54:55 +0100

    Update vcf prepper to allow inclusion of duplicated ids

--- nextflow/nf_modules/remove_dup_ids.nf
+++ nextflow/nf_modules/remove_dup_ids.nf
@@ -1,9 +1,9 @@
 #!/usr/bin/env nextflow
 
 /*
-* This script post prcess a VCF file after running through VEP. It will -
-* - remove any seq region with PATCH/TEST/CTG in it's name
-* - remove any variant with duplicated rsIDs
+* Post process a VCF file after annotating with VEP. 
+* - remove variant on sequence region with PATCH/TEST/CTG in it's name
+* - remove variant with non-unique identifier
 */
 
 process removeDupIDs {
@@ -16,14 +16,15 @@ process removeDupIDs {
   tuple env(output_file), val(genome), val(source), val(priority), val(index_type)
   
   shell:
-  remove_patch = params.remove_patch
+  remove_nonunique_ids = params.remove_nonunique_ids
+  remove_patch_regions = params.remove_patch_regions
   
   '''
   # format input and output file name
   input_file=!{input_file}
-  output_file=${input_file/_renamed_VEP.vcf.gz/_processed_VEP.vcf.gz}
+  output_file=${input_file/renamed/processed}
   
   pyenv local variation-eva
-  python3 !{moduleDir}/../../src/python/ensembl/scripts/remove_duplicate_ids.py ${input_file} !{remove_patch}
+  python3 !{moduleDir}/../../src/python/ensembl/scripts/remove_duplicate_ids.py ${input_file} !{remove_nonunique_ids} !{remove_patch_regions}
   '''
 }
--- src/python/ensembl/scripts/remove_duplicate_ids.py
+++ src/python/ensembl/scripts/remove_duplicate_ids.py
@@ -1,29 +1,72 @@
 import sys
 from cyvcf2 import VCF, Writer
+from cyvcf2.cyvcf2 import Variant
 
-input_vcf = VCF(sys.argv[1])
-id_count = {}
-for variant in input_vcf:
-    if not variant.ID in id_count:
-        id_count[variant.ID] = False
-    else:
-        id_count[variant.ID] = True
-input_vcf.close()
-
-input_vcf = VCF(sys.argv[1])
-ouput_file = sys.argv[1].replace("renamed", "processed")
-remove_patch = int(sys.argv[2])
-w = Writer(ouput_file, input_vcf)
-for variant in input_vcf:
+def get_id(variant: Variant) -> str:
+    'Get variant id'
     
-    if remove_patch:
-        chr = variant.CHROM
-        if ("CTG" in chr) or ("PATCH" in chr) or ("TEST" in chr):
-            continue
+    return variant.ID
 
-    if id_count[variant.ID]:
-        variant.ID = "."
+def get_positioned_id(variant: Variant) -> str:
+    'Get variant positioned id'
+    
+    return variant.CHROM + ":" + str(variant.POS) + ":" + variant.ID
 
-    w.write_record(variant)
-w.close()
-input_vcf.close()
+def generate_removal_status(vcf_file: str, remove_patch_regions: bool = True) -> dict:
+    'Generate hash against variant about its removal status'
+    
+    removal_status = {}
+    input_vcf = VCF(vcf_file)
+    for variant in input_vcf:
+        variant_identifier = get_identifier(variant)
+        
+        # Order is important here - first, check for patch sequence region and then for uniqueness
+        if remove_patch_regions:
+            chr = variant.CHROM
+            removal_status[variant_identifier] = ("CTG" in chr) or ("PATCH" in chr) or ("TEST" in chr)
+        removal_status[variant_identifier] = variant_identifier in removal_status
+    input_vcf.close()
+    
+    return removal_status
+
+if __name__ == "__main__":
+    '''
+    Removes variant based on uniqueness and sequence region. 
+    About removing variant based on uniqueness -
+        1) By default, variant is discarded if the positioned identifier (chrom:position:id) is same for multiple variant record.
+        2) Optionally, we can ask to remove variant with same ids even if they are in different location (using the remove_nonunique_ids argument).
+        3) When removed, all the variant record is removed. For example, if there is two variant record with same positioned id then both of them
+        will be removed.
+    
+    Usage: python remove_nonunique_ids.py <path> <remove_nonunique_ids> <remove_patch_regions>
+    Options:
+        path                    : full path of the input VCF file.
+        remove_nonunique_ids    : 0 or 1. Whether to remove variant with non-unique variant id.
+        remove_patch_regions    : 0 or 1. Whether to remove variant on patch or contig sequence region 
+    '''
+    
+    input_file = sys.argv[1]
+    remove_nonunique_ids = int(sys.argv[2])
+    remove_patch_regions = int(sys.argv[3])
+    
+    output_file = input_file.replace("renamed", "processed")
+    
+    if remove_nonunique_ids:
+        get_identifier = get_id
+    else:
+        get_identifier = get_positioned_id
+        
+    removal_status = generate_removal_status(input_file, remove_patch_regions)
+    
+    # Remove varinat based on removal status
+    input_vcf = VCF(input_file)
+    output_vcf_writer = Writer(output_file, input_vcf)
+    for variant in input_vcf:
+        
+        variant_identifier = get_identifier(variant)
+        if removal_status[variant_identifier]:
+                continue
+                
+        output_vcf_writer.write_record(variant)
+    output_vcf_writer.close()
+    input_vcf.close()

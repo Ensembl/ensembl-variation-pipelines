d60a1e295cc3d2a506575babc6b39f7c76eed42d
Syed Nakib Hossain <snhossain@ebi.ac.uk>
2024-07-22 18:52:58 +0100

    Use local pylop file

--- nextflow/vcf_prepper/bin/generate_vep_config.py
+++ nextflow/vcf_prepper/bin/generate_vep_config.py
@@ -338,7 +338,7 @@ def generate_vep_config(
                 file.write(f"plugin {plugin}\n")
 
         # PhyloP
-        file.write(f"custom file=https://hgdownload.cse.ucsc.edu/goldenpath/hg38/phyloP100way/hg38.phyloP100way.bw,short_name=phlyoP100way,format=bigwig,type=exact")
+        file.write(f"custom file=/nfs/production/flicek/ensembl/variation/data/DECIPHER/hg38.phyloP100way.bw,short_name=phlyoP100way,format=bigwig,type=exact")
     
     
 def main(args = None):

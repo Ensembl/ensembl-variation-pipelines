519fcad74207583245e5643942db3eb1966724cc
Syed Nakib Hossain <snhossain@ebi.ac.uk>
2025-08-27 10:12:37 +0100

    Enable gencode primary in VEP config

--- nextflow/vcf_prepper/bin/generate_vep_config.py
+++ nextflow/vcf_prepper/bin/generate_vep_config.py
@@ -635,6 +635,7 @@ def main(args=None):
         file.write("variant_class 1\n")
         file.write("protein 1\n")
         file.write("transcript_version 1\n")
+        file.write("gencode_primary 1\n")
 
         if args.cache_dir:
             file.write(f"cache_version {version}\n")
